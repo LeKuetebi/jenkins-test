d14397f327fa8678e8f4609c109f7e58d20351e0
Brilland Nkuete <brilland.nkuete@sucam.cm>
2021-11-14 13:57:21 -0500

    First commit

---
+++ jenkins-Simple-Project/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Brilland");
+		
+		if(args.length!=0) {
+			System.out.println("Thank you for the input: "+Arrays.toString(args));
+		}
+	}
+
+}
